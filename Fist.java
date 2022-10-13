08d6371e4a9d86c078b71206a38af20e8304839e
RickardApel <115081077+RickardApel@users.noreply.github.com>
2022-10-13 09:17:44 +0200

    Add files via upload

---
+++ Fist.java
@@ -0,0 +1,18 @@
+package games;
+
+import java.util.*;
+
+public class Fist {
+    public static final int ROCK = 1;
+    public static final int PAPER = 2;
+    public static final int SCISSORS = 3;
+    private Random random;
+
+    Fist() {
+        random = new Random();
+    }
+
+    public int play() {
+        return (int) (Math.floor(Math.random() * 3) + 1);
+    }
+}
