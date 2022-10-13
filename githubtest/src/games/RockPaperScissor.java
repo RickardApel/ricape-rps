b88c126fc94989c197d7d5d2f3bd256d84740f1a
Rickard <prod.apel@gmail.com>
2022-10-13 13:29:30 +0200

    testcommit

---
+++ githubtest/src/games/RockPaperScissor.java
@@ -0,0 +1,12 @@
+package games;
+
+import java.util.Scanner;
+
+public class RockPaperScissor {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+        System.out.println("Rock(1), Paper(2), Scissor(3)?");
+
+    }
+
+}
